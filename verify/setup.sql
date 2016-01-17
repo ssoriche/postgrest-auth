941d50d161f60bad83d615b6f73a96e8ab759c80
Shawn Sorichetti <ssoriche@gmail.com>
2016-01-16 19:42:07 -0500

    Remove uuid-ossp extension
    pgcrypto includes a function to create uuid's so use that instead of
    requiring another extension.

--- verify/setup.sql
+++ verify/setup.sql
@@ -4,9 +4,6 @@ BEGIN;
 
   SELECT 1/count(*) FROM pg_extension WHERE extname = 'pgcrypto';
   SELECT has_function_privilege('crypt(text,text)', 'execute');
-
-
-  SELECT 1/count(*) FROM pg_extension WHERE extname = 'uuid-ossp';
-  SELECT has_function_privilege('uuid_generate_v4()', 'execute');
+  SELECT has_function_privilege('gen_random_uuid()', 'execute');
 
 ROLLBACK;
