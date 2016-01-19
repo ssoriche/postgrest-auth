4f1322ee337739719c72bf2a4800c272ca8a1d6f
Shawn Sorichetti <ssoriche@gmail.com>
2016-01-18 21:06:42 -0500

    Add users_attributes_base view
    This is the same as the users_base view except it doesn't include the
    ID column so Postgres won't complain about the ID column being NULL.

--- deploy/users_base_view.sql
+++ deploy/users_base_view.sql
@@ -32,4 +32,29 @@ BEGIN;
       WITH CHECK OPTION
   ;
 
+  CREATE OR REPLACE VIEW auth.users_attributes_base AS
+    SELECT
+        username,
+        facebook_token,
+        email,
+        pass,
+        reset_password_token,
+        reset_password_sent_at,
+        remember_created_at,
+        sign_in_count,
+        current_sign_in_at,
+        last_sign_in_at,
+        current_sign_in_ip,
+        last_sign_in_ip,
+        confirmation_token,
+        confirmed_at,
+        confirmation_sent_at,
+        failed_attempts,
+        unlock_token,
+        locked_at,
+        created_at,
+        updated_at
+      FROM auth.users_base
+  ;
+
 COMMIT;
--- revert/users_base_view.sql
+++ revert/users_base_view.sql
@@ -2,6 +2,7 @@
 
 BEGIN;
 
-  DROP VIEW auth.users_base;
+  DROP VIEW IF EXISTS auth.users_base;
+  DROP VIEW IF EXISTS auth.users_attributes_base;
 
 COMMIT;
