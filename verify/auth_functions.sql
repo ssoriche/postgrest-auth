6e70ca8ccd8c023f01a2a19b5ab84c04b1dd10d5
Shawn Sorichetti <ssoriche@gmail.com>
2016-01-27 21:43:54 -0500

    Add verification of auth functions
    Now that there are function definitions, make sure they're accessible
    and executable.

--- verify/auth_functions.sql
+++ verify/auth_functions.sql
@@ -2,6 +2,9 @@
 
 BEGIN;
 
+  SELECT has_function_privilege('auth.check_role_exists()', 'execute');
+  SELECT has_function_privilege('auth.check_user_exists()', 'execute');
+  SELECT has_function_privilege('auth.users_change()', 'execute');
   SELECT has_function_privilege('auth.user_role(text, text)', 'execute');
 
 ROLLBACK;
