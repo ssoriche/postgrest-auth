91d7d26b3ad7b3d8a505636bbd110df3ffcd3cf8
Shawn Sorichetti <ssoriche@gmail.com>
2016-02-13 20:29:19 -0500

    Make update trigger on auth.users work
    The initial trigger did not work at all. This INSTEAD OF UPDATE trigger
    is fully functional (as of right now).

--- deploy/auth_functions.sql
+++ deploy/auth_functions.sql
@@ -86,27 +86,50 @@ BEGIN;
 
   CREATE OR REPLACE FUNCTION auth.users_change() RETURNS trigger AS $auth_users_change$
     DECLARE
-      inputstring TEXT;
+      updatestring TEXT;
       new_record RECORD;
       ret RECORD;
     BEGIN
-      IF tg_op = 'UPDATE' or new.pass <> old.pass THEN
-        IF new.reset_password_token != old.reset_password_token THEN
-          new.reset_password_sent_at = CURRENT_TIMESTAMP;
-        END IF;
-        IF new.confirmation_token != old.confirmation_token THEN
-          new.confirmation_sent_at = CURRENT_TIMESTAMP;
-        END IF;
+      IF new.reset_password_token != old.reset_password_token
+        OR new.reset_password_token IS NOT NULL and old.reset_password_token IS NULL
+      THEN
+        new.reset_password_sent_at = CURRENT_TIMESTAMP;
       END IF;
+      IF new.confirmation_token != old.confirmation_token THEN
+        new.confirmation_sent_at = CURRENT_TIMESTAMP;
+      END IF;
+      IF new.sign_in_count != old.sign_in_count THEN
+        new.last_sign_in_at = old.current_sign_in_at;
+        new.last_sign_in_ip = old.current_sign_in_ip;
+        new.current_sign_in_at = CURRENT_TIMESTAMP;
+      END IF;
+
+      new.updated_at = CURRENT_TIMESTAMP;
+
+      new_record = json_populate_record(
+          null::auth.users_attributes_base, to_json(NEW)
+        )
+      ;
+
+      SELECT string_agg(quote_ident(key) || ' = ' || quote_nullable(value),',') INTO updatestring
+        FROM json_each_text(to_json(new_record)) AS X
+      ;
+
+      EXECUTE 'UPDATE auth.users_base '
+        || 'SET ' || updatestring
+        || 'WHERE id = $1'
+        USING OLD.id
+      ;
+
+      RETURN NEW;
 
     END;
   $auth_users_change$ LANGUAGE plpgsql;
 
-  DROP TRIGGER IF EXISTS auth_users_change ON auth.users_attributes_base;
-  CREATE CONSTRAINT TRIGGER auth_users_change
-    BEFORE DELETE or UPDATE on auth.users_attributes_base
-    FOR EACH ROW
-    EXECUTE PROCEDURE auth.users_change()
+  DROP TRIGGER IF EXISTS auth_users_change ON auth.users;
+  CREATE TRIGGER auth_users_change
+    INSTEAD OF UPDATE ON auth.users
+    FOR EACH ROW EXECUTE PROCEDURE auth.users_change()
   ;
 
   CREATE OR REPLACE FUNCTION
--- revert/auth_functions.sql
+++ revert/auth_functions.sql
@@ -5,6 +5,7 @@ BEGIN;
   DROP TRIGGER IF EXISTS ensure_user_role_exists ON auth.user_roles;
   DROP TRIGGER IF EXISTS ensure_user_user_exists ON auth.user_roles;
   DROP TRIGGER IF EXISTS users_add_trigger on auth.users;
+  DROP TRIGGER IF EXISTS auth_users_change on auth.users;
 
   DROP FUNCTION IF EXISTS auth.user_role(text, text);
   DROP FUNCTION IF EXISTS auth.users_add();
